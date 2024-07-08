2a066f97fc3e00200ee918dceac05b6d8ce9afc3
YuraBarykin <yBarykin@gmail.com>
2024-07-08 17:50:11 +0400

    Переименование

--- Tail.js
+++ Tail.js
@@ -1,6 +1,28 @@
 export class Tail {
-  constructor(x,y) {
-    this.x = x
-    this.y = y
+  constructor(ctx, x, y, size, gap, radius) {
+    this.ctx = ctx
+    this.x = gap + x * (size+gap)
+    this.y = gap + y * (size+gap)
+    this.size = size
+    this.size = size
+    this.radius = radius
+  }
+
+  draw() {
+    this.ctx.beginPath();
+
+    this.ctx.moveTo(this.x + this.radius, this.y);
+    this.ctx.arcTo(this.x + this.size, this.y, this.x + this.size, this.y + this.size, this.radius);
+    this.ctx.arcTo(this.x + this.size, this.y + this.size, this.x, this.y + this.size, this.radius);
+    this.ctx.arcTo(this.x, this.y + this.size, this.x, this.y, this.radius);
+    this.ctx.arcTo(this.x, this.y, this.x + this.size, this.y, this.radius);
+
+    this.ctx.fillStyle = '#CCC'
+    this.ctx.fill()
+    
+    this.ctx.strokeStyle = '#CCC';
+    this.ctx.stroke();
+
+    this.ctx.closePath();
   }
 }
--- sript.js
+++ sript.js
@@ -1,6 +1,6 @@
 
-import { Tail } from "./Tail.js"
 import { Ceil } from "./Ceil.js"
+import { Tail } from "./Tail.js"
 
 const cnv = document.getElementById('canvas')
 const ctx = cnv.getContext('2d')
@@ -14,13 +14,13 @@ const GAP = 20
 const gameBoard = []
 
 for( let i =0; i < GRID_COUNT; i++) {
-  gameBoard.push(new Tail( i % GRID_SIZE, Math.floor(i / GRID_SIZE)))
+  gameBoard.push(new Ceil( i % GRID_SIZE, Math.floor(i / GRID_SIZE)))
 }
 
 const createBackgroundBord = () => {
   gameBoard.forEach(item => {
-    const ceil = new Ceil(ctx, item.x, item.y, CEIL_WIDTH, GAP, 10)
-    ceil.draw()
+    const tail = new Tail(ctx, item.x, item.y, CEIL_WIDTH, GAP, 10)
+    tail.draw()
   })
 }
 
